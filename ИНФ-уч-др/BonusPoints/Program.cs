c853540066bbfabf3aa302b2d5575a82c7c802ce
Kalin1603 <kalintodorov03@gmail.com>
2021-02-10 11:56:41 +0200

    Created project SpeedInformation.csproj and BonusPoints.csproj

---
+++ ИНФ-уч-др/BonusPoints/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace BonusPoints
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.Write("Въведете число: ");
+            int startPoints = int.Parse(Console.ReadLine());
+
+            double bonusPonts = 0.0;
+
+            if (startPoints % 2 == 0)
+            {
+                bonusPonts += 1.0;
+            }
+
+            if (startPoints % 10 == 5)
+            {
+                bonusPonts += 2.0;
+            }
+
+            if (startPoints <= 100)
+            {
+                bonusPonts += 5.0;
+            }
+
+            else if (startPoints > 100 && startPoints < 1000)
+            {
+                bonusPonts = bonusPonts + (startPoints * 0.20);
+            }
+
+            else if (startPoints > 1000)
+            {
+                bonusPonts = bonusPonts + (startPoints * 0.10); ;
+            }
+
+            Console.WriteLine($"Bonus points -> {bonusPonts}");
+
+            double total = startPoints + bonusPonts;
+            Console.WriteLine($"Total -> {total}");
+        }
+    }
+}
---
+++ ИНФ-уч-др/SpeedInformation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SpeedInformation
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            double speed = double.Parse(Console.ReadLine());
+
+            if (speed == 10)
+            {
+                Console.WriteLine("slow");
+            }
+
+            else if (speed > 10 && speed <= 50)
+            {
+                Console.WriteLine("average");
+            }
+
+            else if (speed > 50 && speed <= 150)
+            {
+                Console.WriteLine("fast");
+            }
+
+            else
+            {
+                Console.WriteLine("extremely fast");
+            }
+        }
+    }
+}
