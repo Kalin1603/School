791f294ea9b753f52856f5eaade750a5f4b4777b
Kalin1603 <kalintodorov03@gmail.com>
2021-03-11 10:42:08 +0200

    Added Algorithms.Forms.csproj

---
+++ ИНФ-уч-др/Algorithms.Forms/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿
+namespace Algorithms.Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonEnter2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.textBoxC = new System.Windows.Forms.TextBox();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxA = new System.Windows.Forms.TextBox();
+            this.buttonEnter3 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(77, 89);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(102, 45);
+            this.buttonEnter.TabIndex = 0;
+            this.buttonEnter.Text = "Enter";
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(6, 58);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(67, 172);
+            this.listBox1.TabIndex = 1;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.listBox1);
+            this.groupBox1.Controls.Add(this.buttonEnter);
+            this.groupBox1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 233);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "A)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(77, 150);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Sum negative numbers";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 21);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter numbers. Zero for end";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(79, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 29);
+            this.textBox1.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.buttonEnter2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.listBox2);
+            this.groupBox2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox2.Location = new System.Drawing.Point(503, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(285, 233);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Б)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(79, 150);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(198, 21);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Positive numbers - average";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(79, 25);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(134, 21);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Enter 13 numbers";
+            // 
+            // buttonEnter2
+            // 
+            this.buttonEnter2.Location = new System.Drawing.Point(79, 89);
+            this.buttonEnter2.Name = "buttonEnter2";
+            this.buttonEnter2.Size = new System.Drawing.Size(100, 45);
+            this.buttonEnter2.TabIndex = 6;
+            this.buttonEnter2.Text = "Enter";
+            this.buttonEnter2.UseVisualStyleBackColor = true;
+            this.buttonEnter2.Click += new System.EventHandler(this.buttonEnter2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(79, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 29);
+            this.textBox2.TabIndex = 5;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 21;
+            this.listBox2.Location = new System.Drawing.Point(6, 54);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(67, 172);
+            this.listBox2.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.labelResult);
+            this.groupBox3.Controls.Add(this.textBoxC);
+            this.groupBox3.Controls.Add(this.textBoxB);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Controls.Add(this.label4);
+            this.groupBox3.Controls.Add(this.textBoxA);
+            this.groupBox3.Controls.Add(this.buttonEnter3);
+            this.groupBox3.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.groupBox3.Location = new System.Drawing.Point(118, 251);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(598, 207);
+            this.groupBox3.TabIndex = 4;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "В)";
+            // 
+            // textBoxC
+            // 
+            this.textBoxC.Location = new System.Drawing.Point(63, 157);
+            this.textBoxC.Name = "textBoxC";
+            this.textBoxC.Size = new System.Drawing.Size(100, 33);
+            this.textBoxC.TabIndex = 9;
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(63, 113);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(100, 33);
+            this.textBoxB.TabIndex = 5;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(17, 157);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(21, 25);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "c";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(17, 113);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 25);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "b";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 60);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(22, 25);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "a";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(221, 25);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(167, 25);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Enter the numbers";
+            // 
+            // textBoxA
+            // 
+            this.textBoxA.Location = new System.Drawing.Point(63, 60);
+            this.textBoxA.Name = "textBoxA";
+            this.textBoxA.Size = new System.Drawing.Size(102, 33);
+            this.textBoxA.TabIndex = 2;
+            // 
+            // buttonEnter3
+            // 
+            this.buttonEnter3.Location = new System.Drawing.Point(221, 84);
+            this.buttonEnter3.Name = "buttonEnter3";
+            this.buttonEnter3.Size = new System.Drawing.Size(146, 88);
+            this.buttonEnter3.TabIndex = 1;
+            this.buttonEnter3.Text = "Enter";
+            this.buttonEnter3.UseVisualStyleBackColor = true;
+            this.buttonEnter3.Click += new System.EventHandler(this.buttonEnter3_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(415, 116);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(66, 25);
+            this.labelResult.TabIndex = 5;
+            this.labelResult.Text = "Result:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 467);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonEnter2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxA;
+        private System.Windows.Forms.Button buttonEnter3;
+        private System.Windows.Forms.TextBox textBoxC;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labelResult;
+    }
+}
+
---
+++ ИНФ-уч-др/Algorithms.Forms/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Algorithms.Forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox1.Text);
+            textBox1.Text = string.Empty;
+            if (number != 0)
+            {
+                listBox1.Items.Add(number);
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                buttonEnter.Enabled = false;
+                int[] numbers = new int[listBox1.Items.Count];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = (int)listBox1.Items[i];
+                }
+                label2.Text += $"\n{numbers.Where(x => x < 0).Sum()}";
+            }
+        }
+
+        private void buttonEnter2_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(textBox2.Text);
+            listBox2.Items.Add(number);
+            textBox2.Text = string.Empty;
+            if (listBox2.Items.Count == 13)
+            {
+                textBox2.Enabled = false;
+                buttonEnter2.Enabled = false;
+                int[] numbers = new int[listBox2.Items.Count];
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    numbers[i] = (int)listBox2.Items[i];
+                }
+                label5.Text += $"\n{numbers.Where(x => x > 0 || x < -100).Average():f2}";
+            }
+        }
+
+        private static int Product(int k)
+        {
+            Random random = new Random();
+            int[] numbers = new int[k];
+
+            int product = 1;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = random.Next(-1000, 1000);
+
+                bool isTrue = numbers[i] >= -10 && numbers[i] <= 10;
+                if (isTrue)
+                {
+                    product *= numbers[i];
+                }
+            }
+            return product;
+        }
+
+        private void buttonEnter3_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(textBoxA.Text);
+            int b = int.Parse(textBoxB.Text);
+            int c = int.Parse(textBoxC.Text);
+
+            int result = Product(Math.Abs(c - b)) * Product(a);
+            labelResult.Text = $"{result}";
+        }
+    }
+}
