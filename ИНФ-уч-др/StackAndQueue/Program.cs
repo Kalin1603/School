1df2170a0ea5c3e322f0a99a489bc9d6192fe982
Kalin1603 <kalintodorov03@gmail.com>
2021-04-15 12:23:54 +0300

    Exercises

--- ИНФ-уч-др/StackAndQueue/Program.cs
+++ ИНФ-уч-др/StackAndQueue/Program.cs
@@ -6,20 +6,31 @@ public class Program
 {
     public static void Main()
     {
-        MyQueue<int> numbers = new MyQueue<int>();
-        numbers.Enqueue(1);
-        numbers.Enqueue(2);
-        numbers.Enqueue(3);
-        numbers.Enqueue(4);
-        numbers.Enqueue(5);
-        numbers.Enqueue(6);
+        //MyQueue<int> numbers = new MyQueue<int>();
+        //numbers.Enqueue(1);
+        //numbers.Enqueue(2);
+        //numbers.Enqueue(3);
+        //numbers.Enqueue(4);
+        //numbers.Enqueue(5);
+        //numbers.Enqueue(6);
 
-        while (numbers.Count > 0)
-        {
-            int num = numbers.Dequeue();
-            Console.WriteLine(num);
-            //1, 2, 3, 4, 5, 6  
-        }
+        //while (numbers.Count > 0)
+        //{
+        //    int num = numbers.Dequeue();
+        //    Console.WriteLine(num);
+        //    //1, 2, 3, 4, 5, 6  
+        //}
+
+        MyStack<int> numbers = new MyStack<int>();
+        MyStack<string> words = new MyStack<string>();
+
+        words.Push("C#");
+        words.Push(".Net");
+        words.Push("PHP");
+        words.Push("JS");
+        Console.WriteLine(words.Pop());
+        Console.WriteLine(words.Pop());
+        Console.WriteLine(words.Count);
     }
 }
 
